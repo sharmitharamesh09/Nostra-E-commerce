713de6124b3f10118d86eec2afe7809b8d8ca08b
Sharmitha <Sharmitharamesh09@gmail.com>
2025-07-18 15:09:33 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        id:1,
+        name:"Floral Summer Shirt",
+        src:"products/pic (1).jpg",
+        desc:"",
+        price:1220,
+        tags:["new","blue","summer"]
+    },
+
+    {
+        id:2,
+        name:"Summer Green",
+        src:"products/pic (2).jpg",
+        desc:"",
+        price:1260,
+        tags:["new","green","beach"]
+    },
+
+    {
+        id:3,
+        name:"Party Floral Shirt",
+        src:"products/pic (3).jpg",
+        desc:"",
+        price:1399,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:4,
+        name:"Floral Summer Shirt",
+        src:"products/pic (4).jpg",
+        desc:"",
+        price:1399,
+        tags:["old","white","beach"]
+    },
+    {
+        id:5,
+        name:"Beach Shirt",
+        src:"products/pic (5).jpg",
+        desc:"",
+        price:1579,
+        tags:["old","blue","beach"]
+    }
+,
+    {
+        id:6,
+        name:"Shirt Party Red",
+        src:"products/pic (6).jpg",
+        desc:"",
+        price:1579,
+        tags:["old","red","party"]
+    },
+
+    {
+        id:7,
+        name:"Jeans Shirt",
+        src:"products/pic (13).jpg",
+        desc:"",
+        price:1399,
+        tags:["old","blue","casuals"]
+    },
+    {
+        id:8,
+        name:" White Shirt",
+        src:"products/pic (11).jpg",
+        desc:"",
+        price:1579,
+        tags:["old","white","formals"]
+    },
+    {
+        id:9,
+        name:"Brown Shirt",
+        src:"products/pic (15).jpg",
+        desc:"",
+        price:1220,
+        tags:["new","brown","formals"]
+    },
+
+
+]
+
+export {products}
